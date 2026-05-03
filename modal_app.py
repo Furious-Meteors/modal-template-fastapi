839b6e13f466f1800c752d81dea05b431e3e74cc
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2026-05-03 15:12:44 +0530

    fixing comments

--- modal_app.py
+++ modal_app.py
@@ -2,16 +2,15 @@ import modal
 
 from modal_common import build_fastapi_config, get_env_config
 
-# ✅ correct way to get Modal env
+# SETTING MODAL ENVIRONMENT
 MODAL_ENV = modal.config.get("environment") or "dev"
-
 env_config = get_env_config(MODAL_ENV)
 
+# SETTING MODAL APP
 APP_NAME = f"{env_config.app_name}-{env_config.env_name}"
-
 app = modal.App(APP_NAME)
 
-
+# SETTING MODAL PROJECT
 @app.function(**build_fastapi_config(env_config))
 @modal.asgi_app()
 def fastapi_app():
