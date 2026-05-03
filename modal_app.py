2e079c51db24b3a98c488c93df7dcd61f9f95f79
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2026-05-03 15:11:33 +0530

    adding fix for app name

--- modal_app.py
+++ modal_app.py
@@ -1,15 +1,16 @@
-import os
 import modal
 
 from modal_common import build_fastapi_config, get_env_config
 
-MODAL_ENV = os.getenv("MODAL_ENV", "prod")
+# ✅ correct way to get Modal env
+MODAL_ENV = modal.config.get("environment") or "dev"
+
 env_config = get_env_config(MODAL_ENV)
 
-# 🔥 dynamic naming
 APP_NAME = f"{env_config.app_name}-{env_config.env_name}"
 
 app = modal.App(APP_NAME)
+
 
 @app.function(**build_fastapi_config(env_config))
 @modal.asgi_app()
